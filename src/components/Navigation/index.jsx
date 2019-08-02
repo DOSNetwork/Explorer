f553e3095a09f4549250622735f1212ed93d1c6e
jonnyhsy <jonnyhsy@foxmail.com>
2019-08-02 21:15:02 +0800

    add logo links to /. Make disabled cursor to work

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -7,7 +7,7 @@ export default class Navigation extends Component {
             <div className="header__wrapper">
                 <div className="header__container layout__container">
                     <div className="logo__wrapper">
-                        <img src="/logo.png" className="logo--img" alt="logo" />
+                        <a href="/"><img src="/logo.png" className="logo--img" alt="logo" /></a>
                     </div>
                     <div className="navi__wrapper">
                         <div className="navi__container">
