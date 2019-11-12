7d8421c57d7fd5033ec7b1ada97c3075f82872a7
zhangjunbin <178945494@qq.com>
2019-11-12 10:33:32 +0800

    add node list new node

--- src/pages/NodeList/nodeList.jsx
+++ src/pages/NodeList/nodeList.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { Table } from 'antd'
-import axios from 'axios';
+import NewNode from './newNode'
 import './style.scss';
 import numeral from 'numeral';
 import { DOS_ABI, DOS_CONTRACT_ADDRESS } from '../../util/const'
 const { Column } = Table
 const nodeColumnRender = (text, record, index) => {
     let link = `/nodedetail/${record.node}`;
-    return <><img className="nodelist-avatar" src={record.avatar} alt="avatar" /><Link className='node-detail' to={link}>{text}</Link></>
+    // <img className="nodelist-avatar" src={record.avatar} alt="avatar" />
+    return <>
+        <Link className='node-detail' to={link}>{text}</Link></>
 }
 const numberFormatRender = (text, record, index) => {
     return numeral(text).format('0,0')
@@ -29,7 +31,7 @@ export default class NodeList extends Component {
         }
     }
     componentDidMount() {
-        this.loadNodeList();
+        // this.loadNodeList();
     }
     getSnapshotBeforeUpdate(prevProps) {
         let userLogined = (prevProps.contract.userAddress === '' && this.props.contract.userAddress)
@@ -37,7 +39,7 @@ export default class NodeList extends Component {
     }
     componentDidUpdate(prevProps, preState, snapShot) {
         if (snapShot.userLogined) {
-            this.loadNodeList()
+            // this.loadNodeList()
         }
     }
     loadNodeList = async () => {
@@ -88,65 +90,68 @@ export default class NodeList extends Component {
     render() {
         let { isMetaMaskLogin } = this.props.contract
         return (
-            <Table rowKey={record => record.node}
-                loading={this.state.loading}
-                dataSource={this.state.dataList}
-                pagination={false}
-                rowClassName={(row, index) => {
-                    return index % 2 === 0 ? 'row-light' : 'row-dark'
-                }}>
-                <Column title="Node"
-                    render={nodeColumnRender}
-                    dataIndex="node"
-                    key="node" />
-                <Column title="Self Staked"
-                    render={numberFormatRender}
-                    dataIndex="selfStaked"
-                    key="selfStaked"
-                    sorter={(a, b) => a.selfStaked - b.selfStaked}
-                    sortDirections={['ascend', 'descend']} />
-                <Column title="Total Delegated"
-                    render={numberFormatRender}
-                    dataIndex="totalDelegated"
-                    key="totalDelegated"
-                    sorter={(a, b) => a.totalDelegated - b.totalDelegated}
-                    sortDirections={['ascend', 'descend']} />
-                <Column title="Reward Cut"
-                    render={(t) => `${t}%`}
-                    dataIndex="rewardCut"
-                    key="rewardCut"
-                    sorter={(a, b) => a.rewardCut - b.rewardCut}
-                    sortDirections={['ascend', 'descend']} />
-                <Column title="Total Rewards"
-                    render={numberFormatRender}
-                    dataIndex="totalRewards"
-                    key="totalRewards"
-                    sorter={(a, b) => a.totalRewards - b.totalRewards}
-                    sortDirections={['ascend', 'descend']} />
-                <Column title="Uptime"
-                    render={(t) => `${t} days`}
-                    dataIndex="uptime"
-                    key="uptime"
-                    sorter={(a, b) => a.uptime - b.uptime}
-                    sortDirections={['ascend', 'descend']} />
-                {
-                    isMetaMaskLogin ? <Column title="MyDelegation"
-                        render={myDelegationFormatRender}
-                        dataIndex="myDelegation"
-                        key="myDelegation"
-                        sorter={(a, b) => a.myDelegation - b.myDelegation}
-                        sortDirections={['ascend', 'descend']} /> : null
-                }
-                {
-                    isMetaMaskLogin ? <Column title="MyRewards"
-                        render={myDelegationFormatRender}
-                        dataIndex="myRewards"
-                        key="myRewards"
-                        sorter={(a, b) => a.myRewards - b.myRewards}
-                        sortDirections={['ascend', 'descend']} /> : null
-                }
+            <>
+                <NewNode></NewNode>
+                <Table rowKey={record => record.node}
+                    loading={this.state.loading}
+                    dataSource={this.state.dataList}
+                    pagination={false}
+                    rowClassName={(row, index) => {
+                        return index % 2 === 0 ? 'row-light' : 'row-dark'
+                    }}>
+                    <Column title="Node"
+                        render={nodeColumnRender}
+                        dataIndex="node"
+                        key="node" />
+                    <Column title="Self Staked"
+                        render={numberFormatRender}
+                        dataIndex="selfStaked"
+                        key="selfStaked"
+                        sorter={(a, b) => a.selfStaked - b.selfStaked}
+                        sortDirections={['ascend', 'descend']} />
+                    <Column title="Total Delegated"
+                        render={numberFormatRender}
+                        dataIndex="totalDelegated"
+                        key="totalDelegated"
+                        sorter={(a, b) => a.totalDelegated - b.totalDelegated}
+                        sortDirections={['ascend', 'descend']} />
+                    <Column title="Reward Cut"
+                        render={(t) => `${t}%`}
+                        dataIndex="rewardCut"
+                        key="rewardCut"
+                        sorter={(a, b) => a.rewardCut - b.rewardCut}
+                        sortDirections={['ascend', 'descend']} />
+                    <Column title="Total Rewards"
+                        render={numberFormatRender}
+                        dataIndex="totalRewards"
+                        key="totalRewards"
+                        sorter={(a, b) => a.totalRewards - b.totalRewards}
+                        sortDirections={['ascend', 'descend']} />
+                    <Column title="Uptime"
+                        render={(t) => `${t} days`}
+                        dataIndex="uptime"
+                        key="uptime"
+                        sorter={(a, b) => a.uptime - b.uptime}
+                        sortDirections={['ascend', 'descend']} />
+                    {
+                        isMetaMaskLogin ? <Column title="MyDelegation"
+                            render={myDelegationFormatRender}
+                            dataIndex="myDelegation"
+                            key="myDelegation"
+                            sorter={(a, b) => a.myDelegation - b.myDelegation}
+                            sortDirections={['ascend', 'descend']} /> : null
+                    }
+                    {
+                        isMetaMaskLogin ? <Column title="MyRewards"
+                            render={myDelegationFormatRender}
+                            dataIndex="myRewards"
+                            key="myRewards"
+                            sorter={(a, b) => a.myRewards - b.myRewards}
+                            sortDirections={['ascend', 'descend']} /> : null
+                    }
 
-            </Table>
+                </Table>
+            </>
         )
     }
 }
