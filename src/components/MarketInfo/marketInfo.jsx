7586b31deb92d80479f0a7b776377a98cc47bc4a
ericnien <nien0223@gmail.com>
2019-12-10 10:13:20 -0800

    fix:interest rate and number of staked token was reverted by mergeing
    Signed-off-by: ericnien <nien0223@gmail.com>

--- src/components/MarketInfo/marketInfo.jsx
+++ src/components/MarketInfo/marketInfo.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { injectIntl } from 'react-intl'
-import axios from 'axios'
-import './style.scss';
+import React, { Component } from "react";
+import { injectIntl } from "react-intl";
+import axios from "axios";
+import "./style.scss";
 import { DOS_ABI, DOS_CONTRACT_ADDRESS } from "../../util/const";
 import interestRateIcon from "./assets/interestRate-icon.png";
 import priceIcon from "./assets/price-icon.png";
@@ -11,10 +11,10 @@ const Navigation = class Navigation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dosPrice: '-',
-      currency: '',
-      numberOfStaked: '-',
-      interestRate: '-'
+      dosPrice: "-",
+      currency: "",
+      numberOfStaked: "-",
+      interestRate: "-"
     };
   }
   componentDidMount() {
@@ -38,64 +38,63 @@ const Navigation = class Navigation extends Component {
       DOS_CONTRACT_ADDRESS
     );
     const rate = await contractInstance.methods.getCurrentAPR().call();
-    const totalStakedTokens = await contractInstance.methods.totalStakedTokens().call();
-    console.log("rate ", rate.toString(), "token ", (totalStakedTokens));
+    const totalStakedTokens = await contractInstance.methods
+      .totalStakedTokens()
+      .call();
     if (!this.unMount) {
       this.setState({
-        interestRate: rate.toString(),
-        numberOfStaked: 'ERROR'
-      })
+        interestRate: Math.round(rate.toString()) / 10000,
+        numberOfStaked: fromWei(totalStakedTokens)
+      });
     }
-  }
+  };
   fetchDosPrice = () => {
-    let address = '0x70861e862e1ac0c96f853c8231826e469ead37b1',
-      currency = 'usd'
-    axios.get(`https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${address}&vs_currencies=${currency}`).then(({ data }) => {
-      if (data && data[address] && data[address][currency]) {
-        this.setState({
-          dosPrice: data[address][currency],
-          currency: currency
-        })
-      }
-    })
-  }
+    let address = "0x70861e862e1ac0c96f853c8231826e469ead37b1",
+      currency = "usd";
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${address}&vs_currencies=${currency}`
+      )
+      .then(({ data }) => {
+        if (data && data[address] && data[address][currency]) {
+          this.setState({
+            dosPrice: data[address][currency],
+            currency: currency
+          });
+        }
+      });
+  };
   render() {
-    let {
-      dosPrice,
-      numberOfStaked,
-      interestRate, currency
-    } = this.state;
+    let { dosPrice, numberOfStaked, interestRate, currency } = this.state;
     let { formatMessage: f } = this.props.intl;
     return (
       <div className="market-info--wrapper">
         <div className="market-info--block block--rate">
-          <p className='block--title'>
-            <img width='20' src={interestRateIcon} alt="icon" /><span>{f({ id: 'Tooltip.MarketInfo.InterestRate' })}</span>
+          <p className="block--title">
+            <img width="20" src={interestRateIcon} alt="icon" />
+            <span>{f({ id: "Tooltip.MarketInfo.InterestRate" })}</span>
           </p>
-          <p className="block--value">
-            {interestRate}
-          </p>
+          <p className="block--value">{interestRate}</p>
         </div>
         <div className="market-info--block block--token">
-          <p className='block--title'>
-            <img width='20' src={stakedTokenIcon} alt="icon" /><span>{f({ id: 'Tooltip.MarketInfo.NumberOfStakedToken' })}</span>
+          <p className="block--title">
+            <img width="20" src={stakedTokenIcon} alt="icon" />
+            <span>{f({ id: "Tooltip.MarketInfo.NumberOfStakedToken" })}</span>
           </p>
-          <p className="block--value">
-            {numberOfStaked}
-          </p>
+          <p className="block--value">{numberOfStaked}</p>
         </div>
         <div className="market-info--block block--price">
-          <p className='block--title'>
-            <img width='20' src={priceIcon} alt="icon" /><span>{f({ id: 'Tooltip.MarketInfo.PriceOfDOS' })}({currency})</span>
+          <p className="block--title">
+            <img width="20" src={priceIcon} alt="icon" />
+            <span>
+              {f({ id: "Tooltip.MarketInfo.PriceOfDOS" })}({currency})
+            </span>
           </p>
-          <p className="block--value">
-            {dosPrice}
-          </p>
+          <p className="block--value">{dosPrice}</p>
         </div>
       </div>
-    )
+    );
   }
 };
 
-
-export default injectIntl(Navigation)
+export default injectIntl(Navigation);
